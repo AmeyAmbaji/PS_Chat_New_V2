e696e443968d62a87c0034e9b3462914aa6bea4c
Dapper Data Pty Ltd <amey@dapperdata.com.au>
2021-10-10 02:14:26 +1100

    Account search update
    Account search update

--- account/views.py
+++ account/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
 
 from account.forms import RegistrationForm, AccountAuthenticationForm
+from account.models import Account
 
+# This is basically almost exactly the same as friends/friend_list_view
+def account_search_view(request, *args, **kwargs):
+	context = {}
+	if request.method == "GET":
+		search_query = request.GET.get("q")
+		if len(search_query) > 0:
+			search_results = Account.objects.filter(email__icontains=search_query).filter(username__icontains=search_query).distinct()
+			user = request.user
+			accounts = [] # [(account1, True), (account2, False), ...]
+			for account in search_results:
+				accounts.append((account, False)) # you have no friends yet
+			context['accounts'] = accounts
+				
+	return render(request, "account/search_results.html", context)
 
 def register_view(request, *args, **kwargs):
 	user = request.user
@@ -74,3 +90,43 @@ def get_redirect_if_exists(request):
 		if request.GET.get("next"):
 			redirect = str(request.GET.get("next"))
 	return redirect
+
+
+
+
+def account_view(request, *args, **kwargs):
+	"""
+	- Logic here is kind of tricky
+		is_self (boolean)
+			is_friend (boolean)
+				-1: NO_REQUEST_SENT
+				0: THEM_SENT_TO_YOU
+				1: YOU_SENT_TO_THEM
+	"""
+	context = {}
+	user_id = kwargs.get("user_id")
+	try:
+		account = Account.objects.get(pk=user_id)
+	except:
+		return HttpResponse("Something went wrong.")
+	if account:
+		context['id'] = account.id
+		context['username'] = account.username
+		context['email'] = account.email
+		context['profile_image'] = account.profile_image.url
+		context['hide_email'] = account.hide_email
+
+		# Define template variables
+		is_self = True
+		is_friend = False
+		user = request.user
+		if user.is_authenticated and user != account:
+			is_self = False
+		elif not user.is_authenticated:
+			is_self = False
+			
+		# Set the template variables to the values
+		context['is_self'] = is_self
+		context['is_friend'] = is_friend
+		context['BASE_URL'] = settings.BASE_URL
+		return render(request, "account/account.html", context)
